0b9f3fa2632343b6091dae19a8a5228f0ca9e8eb
Rafael Duarte Pereira <72765074+RafaelDuarteP@users.noreply.github.com>
2020-10-31 00:49:36 -0300

    Adicionando pasta de scripts

---
+++ Scripts/images.js
@@ -0,0 +1,95 @@
+let cidades = {
+    cidade1: 'Belo Horizonte',
+    cidade2: 'Ouro Preto',
+    cidade3: 'Mariana',
+    cidade4: 'Tiradentes',
+    cidade5: 'Carrancas',
+    cidade6: 'Diamantina',
+    cidade7: 'Brumadinho',
+    cidade8: 'Monte Verde',
+    cidade9: 'Cordisburgo',
+    cidade10: 'Poços de Caldas',
+}
+
+let beloHorizonte = {
+    img1:'<img src="https://lh3.googleusercontent.com/proxy/WxOEzxIEMLaJF6yGHOQrvQ9ILES05eydrScGA7EJyeCoZSAvMVMwowux_YAfPlSV8b-jVtERDW6l8keOrxdYC9F1pylvfjxhUgtkVlimsmXY" alt="Vista aérea mineirão">',
+    img2:'<img src="https://content.skyscnr.com/m/36952f0d33b958e7/original/GettyImages-186133603.jpg?resize=1800px:1800px&quality=100" alt="Igreja São Francisco de Assis (Igrejinha da Pampulha)">',
+    img3:'<img src="https://www.pracadopapa.com.br/wp-content/uploads/2018/06/praca-do-papa-11851976838886621.jpg" alt="Praça do Papa">',
+    img4:'<img src="https://lh3.googleusercontent.com/proxy/Pqy7sPM5cep_4ufgVoAcHtae8w1GGiLP8AEXubdA8rCPRdnWY49B2GAXc5hz-KXiBFwgX8W2X1mBP5vL83V3ZNRWcxsGEszwTGBbYJyyyvaW5BPHn3hqUWQqBDA2z7yiP5iQxTrgz1fTyQpaf8U3CgPPhM2R6M7o1g" alt="Praça da Liberdade">',
+    img5:'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQieThKW2SyVqomSrKyBEL6DFfZ6FAQinTGgg&usqp=CAU" alt="Mercado Central">',
+}
+
+let ouroPreto = {
+    img1:'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLByln7wWIOPeu6C_Q1-9U_AndCMMwP6_Nog&usqp=CAU" alt="Igreja com serra ao fundo">',
+    img2:'<img src="https://cdn-istoe-ssl.akamaized.net/wp-content/uploads/sites/14/2020/04/ouro-preto.jpg" alt="Igreja com serra ao fundo">',
+    img3:'<img src="https://spguia.melhoresdestinos.com.br/system/fotos_local/fotos/34201/show/igreja-de-sao-francisco-de-assis.jpg" alt="Igreja de São Francisco de Assis">',
+    img4:'<img src="https://i.pinimg.com/originals/bb/5d/3b/bb5d3b5dc211d0924dfa157f27530ced.jpg" alt="Vista Superior da Cidade">',
+    img5:'<img src="https://partiupelomundo.com/wp-content/uploads/2016/04/Ouro-Preto-Brasil-Minas-e1459627063258.jpg" alt="Vista da rua">',
+}
+
+let mariana = {
+    img1:'<img src="https://www.fundacaorenova.org/wp-content/uploads/2019/03/saude-vocal-mariana-site.jpg" alt="Vista aérea">',
+    img2:'<img src="https://cdnstatic8.com/viagensecaminhos.com//wp-content/uploads/2010/09/mariana-minas-gerais.jpg" alt="Duas Igrejas">',
+    img3:'<img src="https://www.fundacaorenova.org/wp-content/uploads/2019/04/rodada-negocios-mariana-site.jpg" alt="Vista da rua">',
+    img4:'<img src="https://spguia.melhoresdestinos.com.br/system/fotos_local/fotos/34413/show/mariana.jpg" alt="Igreja">',
+    img5:'<img src="https://outrosrelatos.com.br/wp-content/uploads/2017/04/pra%C3%A7a-da-se-mariana.jpg" alt="Praça da Sé">',
+}
+
+let tiradentes = {
+    img1:'<img src="https://www.pacodolavradio.com.br/wp-content/uploads/2018/06/igreja-matriz-de-santo-antonio-tiradentes-mg.jpg" alt="Igreja Matriz de Santo Antonio">',
+    img2:'<img src="https://ladica.com.br/wp-content/uploads/2016/04/Cidade-De-Tiradentes-a-beleza-da-arquitetura-Barroca-1050x743.jpg" alt="Subida para Igreja Matriz">',
+    img3:'<img src="https://lirp-cdn.multiscreensite.com/36e30871/dms3rep/multi/opt/igreja-nossa-senhora-do-rosc3a1rio-640w.jpg" alt="Igreja Nossa Senhora do Rosario">',
+    img4:'<img src="https://www.vli-logistica.com.br/wp-content/uploads/2017/12/Trem_Turistico_VLI-10-ajustes.jpg" alt="Maria Fumaça">',
+    img5:'<img src="https://irp-cdn.multiscreensite.com/36e30871/dms3rep/multi/Chafariz+Tiradentes.jpg" alt="Chafariz de São José">',
+}
+
+let carrancas = {
+    img1:'<img src="https://turismodeminas.com.br/wp-content/uploads/2017/01/IMG_8570.jpg" alt="Centro histórico">',
+    img2:'<img src="https://i.pinimg.com/originals/fc/d3/ab/fcd3abfc94194369173bc73b5fd34e7a.jpg" alt="Cachoeira da Esmeralda">',
+    img3:'<img src="https://www.carrancas.com.br/cachoeiras/images/portfolio_toca/12_g.jpg" alt="Poço do Coração">',
+    img4:'<img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Cachoeira_da_Fuma%C3%A7a_%28Carrancas%29.jpg" alt="Cachoeira da Fumaça">',
+    img5:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/0f/01/a7/98/cachoeira-do-salomao.jpg" alt="Cachoeira do Salomão">',
+}
+
+let diamantina = {
+    img1:'<img src="https://magazine.zarpo.com.br/wp-content/uploads/2019/02/Roteiro-de-1-Dia-em-Diamantina-1.jpg" alt="Catedral de Santo Antônio da Sé">',
+    img2:'<img src="https://www.minasecoturismo.com.br/wp-content/uploads/2018/08/Mercado-Velho-em-Diamantina-MG-1.jpg" alt="Mercado Velho">',
+    img3:'<img src="https://www.minasgerais.com.br/imagens/atracoes/1504184891nnUWbQFHII.jpg" alt="Casa de Juscelino Kubitschek">',
+    img4:'<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Igreja_da_Ordem_Terceira_de_Nossa_Senhora_do_Monte_do_Carmo.JPG/1024px-Igreja_da_Ordem_Terceira_de_Nossa_Senhora_do_Monte_do_Carmo.JPG" alt="Igreja de Nossa Senhora do Carmo">',
+    img5:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/0d/06/7d/71/casa-da-xica-da-silva.jpg" alt="Casa de Chica da Silva">',
+}
+
+let brumadinho = {
+    img1:'<img src="https://diariodocomercio.com.br/wp-content/uploads/2019/02/Inhotim.jpg" alt="Inhotim">',
+    img2:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/0c/5f/41/30/igreja-nossa-senhora.jpg" alt="Igreja Nossa Senhor da Piedade">',
+    img3:'<img src="https://lh3.googleusercontent.com/proxy/EvupncCNkAODwx30B1x5xZZ6ko6R5Knp5aL1jo9MZbl34I7XhkjIAR46R7gtyhbJ4eeo_HeZgE_c7-6Ocplbl5G-haBeFjCevi62mlHUaRqC0ZI0dMeDQZ8F4Kr1tfBp1QxmCdYy4nXIiFSuR-ejefdJRimtuMrFM_tHC1NMUemeBLkFAY-nD07fwISLYL4dOre6Hsubm2aZsCrjDUoQqjASKLWJAJObcA" alt="Fazenda dos Martins">',
+    img4:'<img src="https://lh3.googleusercontent.com/proxy/0iv1nsAjWuptujmZuTI3s7mMqRhrG_X7MhB2N1VrhJdTz_QgHgzr_7qTLSt6V9G8Jc-gQ9-hgqwzMLrnRMA0fJXwnSorTYbshfbroEbYaoNYL7q6yYNmvN5mAbP0c5WXYgoZo4XJhSgEZMJzkUfof7W3a-wguRF6ngj2QpbuOnDMqZRULrTr6mbPuAkN3VqJQw-aBSna7-YeFsyWNAWJ11n_71eSWA" alt="Serra da Calçada">',
+    img5:'<img src="https://viajento.files.wordpress.com/2020/06/cachoeira-da-ostra-casa-branca-brumadinho.jpg" alt="Cachoeira da Ostra">',
+}
+
+let monteVerde = {
+    img1:'<img src="https://i2.wp.com/documentodeviagem.com/wp-content/uploads/2020/02/Shopping-Center-Celeiro-monte-verde-documento-de-viagem-5_edited.jpg?resize=800%2C445&ssl=1" alt="Shopping Celeiro">',
+    img2:'<img src="https://img.r7.com/images/2014/03/14/3l6jzgilrf_4w8osdq4bu_file.jpg?dimensions=660x360" alt="Vista da rua">',
+    img3:'<img src="https://monteverde.org.br/wp-content/uploads/2017/06/Pedra-Redonda-Drone-850x491.jpg" alt="Pedra redonda">',
+    img4:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/06/f6/ac/b3/igreja-de-sao-francisco.jpg" alt="Igreja de São Francisco de Assis">',
+    img5:'<img src="https://cdnstatic8.com/viajandonajanela.com//wp-content/uploads/2019/03/monte-verde-mg-o-que-fazer-vnj-1-de-1-4.jpg" alt="Vila Europa">',
+}
+
+
+let cordisburgo = {
+    img1:'<img src="https://www.transportal.com.br/noticias/wp-content/uploads/2018/11/Casa-Elefante-em-Cordisburgo.jpg" alt="Casa Elelefante">',
+    img2:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/09/38/e2/45/capela-sao-jose.jpg" alt="Capela São José">',
+    img3:'<img src="https://www.casadedoda.com/wp-content/uploads/2017/07/cordisburgo-mg-igreja-matriz-sagrado-coracao-2.jpg" alt="igreja matriz">',
+    img4:'<img src="https://www.viagenspossiveis.com.br/wp-content/uploads/2018/05/Gruta-do-Maquine-MG-8.jpg" alt="Gruta de Maquiné">',
+    img5:'<img src="https://i1.wp.com/www.conhecendomuseus.com.br/wp-content/uploads/2016/06/Fachada-1-1.png?resize=748%2C350" alt="Museu Cas Guimarães Rosa">',
+}
+
+
+let pocosDeCaldas = {
+    img1:'<img src="https://media-cdn.tripadvisor.com/media/photo-s/17/9a/15/99/cristo.jpg" alt="Cristo">',
+    img2:'<img src="https://upload.wikimedia.org/wikipedia/commons/0/07/V%C3%A9u_das_Noivas%2C_Po%C3%A7os_de_Caldas_-_MG_%285711207139%29.jpg" alt="Cachoeira Veu das Noivas">',
+    img3:'<img src="https://www.pocosja.com.br/divirta-se/wp-content/uploads/2016/11/1480102458acf3e992fe4f9c440221d7341524ba40.jpg" alt="Fonte dos amores">',
+    img4:'<img src="https://www.feriasbrasil.com.br/fotosfb/696850170-MOB.jpg" alt="Recanto Japones">',
+    img5:'<img src="https://turismopocosdecaldas.com.br/wp-content/uploads/2019/08/po%C3%A7os.jpg" alt="Relogio Floral">',
+}
+
