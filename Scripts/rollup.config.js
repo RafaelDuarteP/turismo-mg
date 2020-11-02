722a7499f473fc2f7bc6bc8ddffa6fd1ff1b2aef
Thalles <thalles6alves@gmail.com>
2020-11-02 17:34:48 -0300

    adiciona estrutura do RollupJS para scripts

---
+++ Scripts/rollup.config.js
@@ -0,0 +1,137 @@
+// Plugins do Rollup (importando)
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
+import resolve from '@rollup/plugin-node-resolve'; 
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import json from '@rollup/plugin-json';
+//import multiInput from 'rollup-plugin-multi-input';
+
+import posthtml from 'rollup-plugin-posthtml-template';
+import include from 'posthtml-include';
+import minifier from 'posthtml-minifier';
+
+import autoprefixer from 'autoprefixer';
+
+
+// Definindo opções dos plugins
+const defaultPlugins = [
+  postcss({
+    extensions: ['scss'],
+    minimize: true,
+    exec: true,
+    plugins: [
+      autoprefixer(),
+    ]
+  }),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true,
+  }),
+  eslint({
+    babelHelpers: 'bundled',
+    exclude: [
+      'node_modules/**',
+      'src/data/**',
+      'src/styles/**',
+      'tests/**',
+    ],
+  }),
+  posthtml({
+    plugins: [include(),  minifier()],
+    template: true  
+  }),
+  json({
+    compact: true,
+    include: '../data/**',
+  }),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: [
+      'node_modules/**'
+    ],
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: false,
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/transform-template-literals',
+    ],
+    babelrc: false,
+  }),
+
+];
+
+
+// Configuração do babel
+const babelOutputConfig = {
+  allowAllFormats: true,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: false,
+      }
+    ]
+  ],
+  plugins: [
+    '@babel/transform-template-literals',
+  ],
+};
+
+
+// Configuração do output
+export default [
+  // Módulo para o browser
+  {
+    input: 'src/scripts/main.js',
+    output: [
+      {
+        file: 'build/js/turismomg.dev.js',
+        format: 'iife',
+        plugins: [
+          getBabelOutputPlugin(babelOutputConfig),
+        ],
+      },
+      {
+        file: 'build/js/turismomg.min.js',
+        format: 'iife',
+        plugins: [
+          getBabelOutputPlugin(babelOutputConfig),
+          terser(),
+        ],
+      },
+    ],
+    watch: {
+      include: 'src/**',
+    },
+    plugins: defaultPlugins,
+  },
+  // Módulos individuais, para teste
+  /*
+  {
+    input: [
+      'src/scripts/components/**.js', 
+      'src/scripts/modules/**', 
+    ],
+    output: {
+      dir: 'tests/bundle',
+      format: 'es',
+      plugins: [
+        getBabelOutputPlugin(babelOutputConfig),
+      ]
+    },
+    watch: {
+      include: 'src/**',
+    },
+    plugins: defaultPlugins.concat([
+      multiInput(),
+    ]),
+  },
+  */
+];
---
+++ Scripts/src/scripts/main.js
@@ -0,0 +1,7 @@
+// ====== Imports
+import cityImages from '../../../data/images.json';
+
+
+cityImages.forEach((city) => {
+  console.log(city);
+});
